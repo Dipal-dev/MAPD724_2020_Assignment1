b4d88d8b7b5450a1b06ec675e96463fc428d8287
dipalpatel913@gmail.com <dipalpatel913@gmail.com>
2020-01-08 20:13:21 -0500

    Initial Commit

---
+++ MAPD724_2020_Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD724_2020_Assignment1
+//
+//  Created by Dipal Patel on 2020-01-08.
+//  Copyright © 2020 Dipal Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
